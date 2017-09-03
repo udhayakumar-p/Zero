3699862dcf3f94baa497ec7ec18b28f32ac842c1
Thilakuthirasamy <thilakuthirasamy@gmail.com>
2017-09-03 15:26:43 +0530

    Update and rename README.md to Zero.java

---
+++ Zero.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Zero
+{
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        int[] arr=new int[5];
+        int max=9999;
+        int n=0,m=0;
+        for(int i=0;i<5;i++)
+        {
+            arr[i]=s.nextInt();
+        }
+        for(int j=0;j<5;j++)
+        {
+            for(int k=j;k<5;k++)
+            {
+                if((arr[k]+arr[j])<max && j!=k)
+                {
+                    max=arr[j]+arr[k];
+                    n=arr[j];
+                    m=arr[k];
+                }
+            }
+        }
+        System.out.println(n+","+m);
+    }
+}
+
