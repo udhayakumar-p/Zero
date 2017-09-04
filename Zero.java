65a151412d52099aabf62f16431bcc9d6cd01372
udhayakumar-p <udhayakumarp505@gmail.com>
2017-09-04 08:59:36 +0530

    Update Zero.java

--- Zero.java
+++ Zero.java
@@ -6,6 +6,7 @@ public class Zero
         Scanner s=new Scanner(System.in);
         int[] arr=new int[5];
         int max=9999;
+        int dif=9999;
         int n=0,m=0;
         for(int i=0;i<5;i++)
         {
@@ -17,13 +18,18 @@ public class Zero
             {
                 if((arr[k]+arr[j])<max && j!=k)
                 {
-                    max=arr[j]+arr[k];
+                    max=Math.abs(arr[j]+arr[k]);
+                    if(Math.abs(max)-0<dif)
+                    {
+                        dif=max;
                     n=arr[j];
                     m=arr[k];
                 }
             }
         }
+        }
         System.out.println(n+","+m);
-    }
 }
+}
+
 
